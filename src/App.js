295d5632156092e34a0f5e4d30ae09f6a307a099
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-10-16 19:40:54 -0400

    Refactored the navigation and the cart component to be using context coming from CartContext

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ShoppingCart from './components/ShoppingCart';
 
 // Context
 import { ProductContext } from './Context/ProductContext';
+import { CartContext } from './Context/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -28,17 +29,16 @@ function App() {
 
 	return (
 		<ProductContext.Provider value={{ products, addItem }}>
-			<div className="App">
-				<Navigation cart={cart} />
+			<CartContext.Provider value={{ cart }}>
+				<div className="App">
+					<Navigation />
 
-				{/* Routes */}
-				<Route exact path="/" component={Products} />
+					{/* Routes */}
+					<Route exact path="/" component={Products} />
 
-				<Route
-					path="/cart"
-					render={() => <ShoppingCart cart={cart} />}
-				/>
-			</div>
+					<Route path="/cart" component={ShoppingCart} />
+				</div>
+			</CartContext.Provider>
 		</ProductContext.Provider>
 		
 	);
